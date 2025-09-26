2eeed51ef12519c1bd0bad9e8990e1d42dc65d7e
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-09-26 21:34:16 +0530

    updated

---
+++ String Compression.js
@@ -0,0 +1,23 @@
+// String Compression
+
+var compress = function(chars) {
+    let write=0;
+    let read=0;
+    while(read<chars.length){
+        let currentChar=chars[read];
+        let count=0;
+        while(read<chars.length && chars[read]===currentChar){
+            read++;
+            count++;
+        }
+        chars[write]=currentChar;
+        write++;
+        if(count>1){
+            for(let digit of String(count)){
+                chars[write]=digit;
+                write++
+            }
+        }
+    }
+    return write;
+};
